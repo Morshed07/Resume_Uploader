29e263fbaf380bc30bdfa155d356cfaa7f6805cd
Morshed <morshednayeem@icloud.com>
2022-09-11 04:10:13 +0600

    1st commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+STATE_CHOISE = (
+("Dhaka","Dhaka"),
+("Rajshahi","Rajshahi"),
+("Chittagong","Chittagong"),
+("Barishal","Barishal"),
+("Khulna","Khulna"),
+("Rangpur","Rangpur"),
+("Sylhet","Sylhet"),
+)
+
+
+
 # Create your models here.
+class Resume(models.Model):
+    name= models.CharField(max_length=70)
+    dob = models.DateField(auto_now=False, auto_now_add=False)
+    gender = models.CharField(max_length=70)
+    locality= models.CharField(max_length=70)
+    city= models.CharField(max_length=100)
+    pin= models.PositiveIntegerField()
+    state = models.CharField(choices= STATE_CHOISE, max_length=100)
+    mobile= models.PositiveIntegerField()
+    email= models.EmailField()
+    job_city=models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='profileimg', blank=True)
+    my_file=models.FileField(upload_to='doc',blank=True)
