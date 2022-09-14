f856eb0727642e6675252597a93f791e1857239a
Morshed <morshednayeem@icloud.com>
2022-09-14 21:07:37 +0600

    finald commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -20,13 +20,13 @@ class ResumeForm(forms.ModelForm):
     job_city= forms.MultipleChoiceField(label='Preferred Job Location',choices=JOB_CITY_CHOICES, widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Resume
-        fields= ['id','name','dob','gender','locality','city','pin','state','mobile','job_city','profile_image','my_file']
+        fields= ['id','name','dob','gender','locality','city','pin','state','mobile','email','job_city','profile_image','my_file']
 
         labels={'name':'Full Name','dob':'Date OF Birth', 'pin':'Pin Code','mobile':'Mobile No.','email': 'Email ID', 'profile_image': 'Profile Image', 'my_file':'Documents'}
 
         widgets = {
             'name':forms.TextInput(attrs={'class':'form-control'}),
-            'dob':forms.DateInput(attrs={'class':'form-control'}),
+            'dob':forms.DateInput(attrs={'class':'form-control','id' :'datepicker'}),
             'locality':forms.TextInput(attrs={'class':'form-control'}),
             'city':forms.TextInput(attrs={'class':'form-control'}),
             'pin':forms.NumberInput(attrs={'class':'form-control'}),
--- myapp/views.py
+++ myapp/views.py
@@ -7,10 +7,17 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         form = ResumeForm()
-        return render(request, 'home.html', {'form': form})
+        candidates = Resume.objects.all()
+        return render(request, 'home.html', {'form': form, 'candidates': candidates})
 
     def post(self, request):
         form = ResumeForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return render(request, 'home.html', {'form': form} )
+
+
+class CandidateView(View):
+    def get(self, request, pk):
+        candidate = Resume.objects.get(pk=pk)
+        return render(request, "candidate.html",{'candidate':candidate})
